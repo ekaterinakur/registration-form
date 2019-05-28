ed1d16b39a5c7998978e0a85a2407158c7b90747
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-05-28 14:56:52 +0300

    Shortened code; with corrections from TA

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('form');
 const fullname = document.getElementById('fullname');
-const errors = document.querySelectorAll('.error');
 const hideInfo = document.getElementById('hide-info');
 const wideInfo = document.getElementById('wide-info');
 const phone = document.getElementById('phone');
@@ -10,77 +9,69 @@ const selectCities = document.querySelector('select[name="city"]');
 const optionsCities = document.querySelectorAll('select[name="city"] option.display-none');
 const submit = document.querySelector('input[type="submit"]');
 
-// Input full name
-fullname.addEventListener('focus', (e) => {
-  e.target.classList.remove('not-valid');
-  e.target.classList.remove('valid');
-});
+// Check inputs 'fullname' and 'phone'
+form.addEventListener('input', checkPhoneAndName);
 
-fullname.addEventListener('blur', () => {
-  const fullnameValue = fullname.value.trim();
-  const wordsNum = fullnameValue.split(' ').length;
+function checkPhoneAndName(e) {
+  if (e.target !== phone && e.target !== fullname) { return; }
 
-  if (wordsNum > 1 && wordsNum < 4) {
-    fullname.classList.remove('not-valid');
-    fullname.classList.add('valid');
-    errors[0].classList.add('display-none');
-  } else {
-    fullname.classList.add('not-valid');
-    errors[0].classList.remove('display-none');
+  const inputType = e.target.dataset.name;
+  let check;
+
+  switch (inputType) {
+    case 'fullname':
+      const fullnameValue = fullname.value.trim();
+      const wordsNum = fullnameValue.split(' ').length;
+      check = (wordsNum > 1) && (wordsNum < 4);
+      break;
+    case 'phone':
+      const onlyNums = phone.value.replace(/[\s\-\(\)]/g, '');
+      const match = onlyNums.match(/^((\+?3)?8)?0\d{9}$/);
+      check = (match !== null);
   }
-});
+
+  if (check) {
+    e.target.classList.remove('not-valid');
+    e.target.classList.add('valid');
+    e.target.nextElementSibling.classList.add('display-none');
+  } else {
+    e.target.classList.add('not-valid');
+  }
+}
+
+form.addEventListener('blur', (e) => {
+  if (e.target !== phone && e.target !== fullname) { return; }
+
+  if (e.target.classList.contains('not-valid')) {
+    e.target.nextElementSibling.classList.remove('display-none');
+  }
+}, true);
 
 // Checkbox - hide wide info (input 'phone' and select 'region'...)
 hideInfo.addEventListener('change', () => {
-  if (hideInfo.checked) {
-    wideInfo.classList.add('display-none');
-  } else {
-    wideInfo.classList.remove('display-none');
-  }
-});
-
-
-// Input phone
-phone.addEventListener('focus', (e) => {
-  e.target.classList.remove('not-valid');
-  e.target.classList.remove('valid');
-});
-
-phone.addEventListener('blur', () => {
-  const onlyNums = phone.value.replace(/[\s\-\(\)]/g, '');
-
-  if (onlyNums.match(/^((\+?3)?8)?0\d{9}$/) !== null) {
-    phone.classList.remove('not-valid');
-    phone.classList.add('valid');
-    errors[1].classList.add('display-none');
-  } else {
-    phone.classList.add('not-valid');
-    errors[1].classList.remove('display-none');
-  }
+    wideInfo.classList.toggle('display-none');
 });
 
 // Select region and city
 selectRegions.addEventListener('change', () => {
   if (selectRegions.selectedIndex > 1) {
     labelCity.classList.remove('display-none');
-    selectCities.classList.remove('display-none');
     selectCities.value = '';
 
     for (let city of optionsCities) {
-        if (city.dataset.region === selectRegions.value.toLowerCase()) {
+      if (city.dataset.region === selectRegions.value.toLowerCase()) {
         city.classList.remove('display-none');
-        } else {
+      } else {
         city.classList.add('display-none');
-        }
+      }
     }  
   } else {
     labelCity.classList.add('display-none');
-    selectCities.classList.add('display-none');
   }
 });
 
 // Make "submit" able after form is complete
-form.addEventListener('change', checkForm);
+form.addEventListener('input', checkForm);
 
 function checkForm() {
   const checkFullName = fullname.classList.contains('valid');
